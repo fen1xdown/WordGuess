423de8ac891ca30ab71549173ffb68c72b6f27ff
Cameron <cameronleecalder@gmail.com>
2019-05-18 21:41:44 -0500

    first commit

---
+++ game.js
@@ -0,0 +1,7 @@
+// Start with words to guess, how many guesses the player gets, player key input etc
+// run function to take user key, run it against the word, see if user key matches
+// if it does, add the key selected to the word
+// if not subtract from guesses
+// if there are no more guesses the player loses
+// if there are no more letters to be guessed the player wins
+
